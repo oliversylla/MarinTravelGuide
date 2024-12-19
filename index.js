406d1e4568b711f3ddc6d6351b15cc1af4b58026
oe <e@e.em>
2024-12-18 19:48:12 -0800

    actually making blackjack p1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
- 
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+if (sum <= 20) {
+    message = "ooooo bros edging it oooo"
+} else if (sum === 21) {
+    message = "good job sigma"
+    hasBlackJack = true
+} else {
+    message = "haha dumb mcidiot face"
+    isAlive = false
+}
+
+console.log(message)
